3a29cb83ee622db98070713a519c543832805e79
s-blais <stephen@s-blais.com>
2020-11-11 14:02:27 -0500

    tests passing

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
 /* Your Code Here */
 
+let createEmployeeRecord = function (empInfoArray) {
+  return {
+    firstName: empInfoArray[0],
+    familyName: empInfoArray[1],
+    title: empInfoArray[2],
+    payPerHour: empInfoArray[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
+
+let createEmployeeRecords = function (empArrays) {
+  return empArrays.map(empArray => createEmployeeRecord(empArray))
+}
+
+let createTimeInEvent = function (dateStamp) {
+  this['timeInEvents'].push({
+    type: 'TimeIn',
+    hour: parseInt(dateStamp.split(' ')[1]),
+    date: dateStamp.split(' ')[0]
+    });
+  return this;
+}
+
+let createTimeOutEvent = function (dateStamp) {
+  this['timeOutEvents'].push({
+    type: 'TimeOut',
+    hour: parseInt(dateStamp.split(' ')[1]),
+    date: dateStamp.split(' ')[0]
+    });
+  return this;
+}
+
+let hoursWorkedOnDate = function (date) {
+  const timeInByDate = this.timeInEvents.find(event =>
+    event.date === date).hour;
+  const timeOutByDate = this.timeOutEvents.find(event =>
+    event.date === date).hour;
+  return (timeOutByDate-timeInByDate)/100;
+}
+
+let wagesEarnedOnDate = function (date) {
+  return hoursWorkedOnDate.call(this, date)*this.payPerHour
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,3 +65,18 @@ let allWagesFor = function () {
 
     return payable
 }
+
+let findEmployeeByFirstName  = function (srcArray, firstName) {
+  return srcArray.find(emp => emp.firstName === firstName)
+}
+
+// let calculatePayroll = function (empRecs) {
+//   return empRecs.reduce(function(total, emp){
+//     return total + allWagesFor.call(emp)}, 0
+//   )
+// }
+
+let calculatePayroll = function (empRecs) {
+  return empRecs.reduce((total, emp) => total + allWagesFor.call(emp), 0)
+}
+
